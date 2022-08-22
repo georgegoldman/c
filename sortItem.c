b1fdb42605443622aa43b68c2e4dd900ea60dffe
georgegoldman <georgegoldmanjohn.o@gmail.com>
2022-08-22 10:24:24 +0100

    adding sortItemsfile

---
+++ sortItem.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("my c programme is running \n");
+    return 0;
+}
+
+int sortItem(x,y,z)
+{
+    int checkerPoint = 0;
+    int numbersInOrder[3];
+    while (true)
+}
