50b90d60b0b9520d99c8d3291fc879ed851e3896
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-08-31 14:44:30 +0100

    created datatypes app

---
+++ datatypes/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int age  = 40;
+    double gpa = 3.7;
+    char grade = 'a';
+    char phrase[] = "what is going on?";
+
+    printf("this is an integer => %d this is a double => %f this is a char grade => %c this is a string %s \n", age, gpa, grade, phrase);
+
+    return 0;
+}
