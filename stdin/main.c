a8e66b59057d42cffc707d6fed5ab29fde86b32d
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-08-31 17:13:50 +0100

    stdin

--- stdin/main.c
+++ stdin/main.c
@@ -3,6 +3,23 @@
 
 int main()
 {
-    printf("Hello world!\n");
+
+    /*
+    double gpa;
+    printf("Enter your gps: ");
+    scanf("%lf", &gpa);
+    printf("you gpa is  %f", gpa);
+    */
+
+    /*
+    char grade;
+    printf("Enter your grade: ");
+    scanf("%c", &grade);
+    printf("Your grade is %c", grade);
+    */
+    char name[20];
+    printf("Enter your grade: ");
+    fgets(name, 20, stdin);
+    printf("Your name is %s", name);
     return 0;
 }
