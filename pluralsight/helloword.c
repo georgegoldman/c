bf153355d10aaabd8d49e4e5bf9d1188a3a7316c
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-09-14 16:49:34 +0100

    understanding c

---
+++ pluralsight/helloword.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: int 0
+ */
+
+int main(int argc, char const *argv[])
+{
+        /* code */
+        char name[] = "Georgegoldman";
+        printf("%s \n", name);
+        return 0;
+}
