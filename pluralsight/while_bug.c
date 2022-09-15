31637b3389142c6576ac17090516e1fd97ce3659
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-09-15 03:09:45 +0100

    building cool stuff

---
+++ pluralsight/while_bug.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * @brief 
+ * main - entry point
+ * Return: int x = 0
+ * 
+ */
+
+int main(int argc, char const *argv[])
+{
+        /* code */
+        int i = 0;
+        while (i <= 10)
+        {
+                /* code */
+                printf("%d \n ", i);
+                i++;
+        }
+        
+
+        return 0;
+}
