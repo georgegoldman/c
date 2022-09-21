2f16a3a786bbc86d1da0a538433882ec578829b8
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-09-21 12:44:22 +0100

    built cool stuff

---
+++ islower/string_toupper.c
@@ -0,0 +1,16 @@
+/**
+ * @brief 
+ * 
+ * @return char* 
+ */
+char *string_toupper(char *str)
+{
+        int index = 0;
+
+        while (str[index++])
+        {
+                if (str[index] >= 'a' && str[index <= 'z'])
+                        str[index] -= 32;
+        }
+        return (str);
+}
